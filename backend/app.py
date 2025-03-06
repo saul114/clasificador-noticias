a1c3467deb4356cb49c1558e00c46002eeb2952d
saul114 <saul011104@gmail.com>
2025-03-06 01:18:12 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -51,4 +51,6 @@ def process_url():
         return jsonify({"error": f"Error al procesar la URL: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8000)
+        import os
+    port = int(os.environ.get("FLASK_RUN_PORT", 8000))
+    app.run(debug=True, host="0.0.0.0", port=port)
