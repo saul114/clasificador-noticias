0b85e712a95320753e0774c8c6e00b1a12598022
saul114 <saul011104@gmail.com>
2025-03-06 01:28:03 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -52,5 +52,5 @@ def process_url():
 
 if __name__ == "__main__":
         import os
-    port = int(os.environ.get("FLASK_RUN_PORT", 8000))
+    port = int(os.environ.get("FLASK_RUN_PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
