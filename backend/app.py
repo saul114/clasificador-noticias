ab3f190547f3b555ca5a6dcf383501578eb295bc
saul114 <saul011104@gmail.com>
2025-03-06 01:09:10 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -51,4 +51,4 @@ def process_url():
         return jsonify({"error": f"Error al procesar la URL: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8000)
